bc9beaa95a1e82f0ca819d136184de634b3c08fc
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-02-02 10:15:46 -0500

    No more overflow in the array! Woo!

--- 1miniproject/controlerDev.c
+++ 1miniproject/controlerDev.c
@@ -4,7 +4,7 @@
 #define SET_POINT 45
 #define K 2
 
-int sum_array(float a[], int num_elements){
+float sum_array(float a[], int num_elements){
     /*Written by:
     Peter H. Anderson, MSU, Feb 21, '97
     http://www.phanderson.com/C/arraysum.html
@@ -22,10 +22,13 @@ int sum_array(float a[], int num_elements){
 int main(){
     //allocate array for sensor readings:
     float readings[TIME_READING_WINDOW]; 
-    
+    //Oust the deamons:
+    for(int i=0; i<TIME_READING_WINDOW; ++i){
+        readings[i]=0;
+    }
     float sensorReading = 5; //pseudo sensor reading, TODO: REPLACE WITH REAL VALUE
     
-    for(int i=1; i<TIME_READING_WINDOW-1; ++i){
+    for(int i=1; i<TIME_READING_WINDOW; ++i){
         //Shift out the oldest sensor reading:
         if(readings[i]>0.1000){
             readings[i-1] = readings[i]; 
@@ -34,17 +37,23 @@ int main(){
             readings[i-1] = sensorReading;
         }
     }
+    
     //Write the current sensor reading to end of array:
     readings[TIME_READING_WINDOW-1] = sensorReading;
     
+    /* Print array values, Debug only.
     for(int i=0; i<=TIME_READING_WINDOW-1; i++){
-        printf("Array value at %2d is: %f \n",i,readings[i]);
-    }
+        printf("Array value at %2d is: %f \n", i, readings[i]);
+    }*/
     
     float sum_sensor_readings = sum_array(readings, 30);
+    
+    //Average sensor readings in time window to get approximate current position:
     float current_position = sum_sensor_readings/TIME_READING_WINDOW; 
     float motor_command = K*(SET_POINT - current_position);
     printf("The sum of the sensor readings is: %6.1f \n", sum_sensor_readings);
+    printf("Current position is: %3.4f\n",current_position);
+    printf("Command for motor is: %3.4f\n",motor_command);
 
     return 0;
 }
